5337b7b28016d2e4c5c90b12e890e9cab0b8cbe7
kunihiko.yaegashi <kunihiko.yaegashi@gmail.com>
2018-05-30 08:48:23 +0000

    add tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(message_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -51,4 +51,5 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content)
+  end
 end
